5c3e138b3a29c824b8a1e70326ea1dee5c16016e
Helena Oliveira <helena@informach.com.br>
2022-07-22 13:40:16 -0300

    add product factory

---
+++ src/domain/product/factory/product.factory.ts
@@ -0,0 +1,22 @@
+import crypto from 'node:crypto';
+
+import Product from '../entity/product';
+import ProductB from '../entity/product-b';
+import ProductInterface from '../entity/product.interface';
+
+export default class ProductFactory {
+  public static create(
+    type: string,
+    name: string,
+    price: number,
+  ): ProductInterface {
+    switch (type) {
+      case 'a':
+        return new Product(crypto.randomUUID(), name, price);
+      case 'b':
+        return new ProductB(crypto.randomUUID(), name, price);
+      default:
+        throw new Error('Product type not supported');
+    }
+  }
+}
