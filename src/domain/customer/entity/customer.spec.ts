88d4a29fde8c1916401bba737726625b4260365d
Helena Oliveira <helena@informach.com.br>
2022-07-22 13:35:02 -0300

    add product entity

--- src/domain/customer/entity/customer.spec.ts
+++ src/domain/customer/entity/customer.spec.ts
@@ -4,13 +4,13 @@ import Customer from './customer';
 describe('Customer unit tests', () => {
   it('should throw error when id is empty', () => {
     expect(() => {
-      const customer = new Customer('', 'John');
+      new Customer('', 'John');
     }).toThrowError('Id is required');
   });
 
   it('should throw error when name is empty', () => {
     expect(() => {
-      const customer = new Customer('123', '');
+      new Customer('123', '');
     }).toThrowError('Name is required');
   });
 
